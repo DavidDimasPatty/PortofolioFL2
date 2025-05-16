918b5e9c274222bd18556976de4484e7877a61e4
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-05-16 22:34:35 +0700

    top adjustment

--- src/components/Top.js
+++ src/components/Top.js
@@ -10,17 +10,19 @@ const Top = () => {
             <div className="row wrapperRowTop">
                 <div className="col-12 col-md-7  titleName">
                     {/* IKODOR<div className="lastLetterTop">A</div> */}
-                    <img src={topLogo} className="imgTopLogo"/>
+                    <img src={topLogo} className="imgTopLogo" />
                 </div>
                 <div className="topRight col-12 col-md-5">
                     <div className="buttonTopContentTitle">
                         <h2>HOW CAN WE HELP?</h2>
                     </div>
-                    <div className="mb-3 buttonTopContent  d-flex justify-content-start align-items-center">
-                        <button> OUR PLAN </button>
-                    </div>
-                    <div className="buttonTopContent  d-flex justify-content-start align-items-center">
-                        <button>  CONTACT US </button>
+                    <div className="wrapperButtonTop">
+                        <div className="mb-3 buttonTopContent">
+                            <button> OUR PLAN </button>
+                        </div>
+                        <div className="buttonTopContent">
+                            <button>  CONTACT US </button>
+                        </div>
                     </div>
                 </div>
             </div>
