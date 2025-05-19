42713efcd71fb5a87b7b91a2dac2efbd548b2c41
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-05-19 21:07:29 +0700

    how this work lock

--- src/components/HowThisWorks.js
+++ src/components/HowThisWorks.js
@@ -107,8 +107,16 @@ const HowThisWorks = () => {
 
             if (rect.top >= 0 && rect.bottom <= windowHeight) {
                 document.body.style.overflow = "hidden";
+                document.body.style.position = 'fixed';
+                document.body.style.top = `-${window.scrollY}px`;
+                document.body.style.left = '0';
+                document.body.style.right = '0';
             } else {
-                document.body.style.overflow = "";
+                document.body.style.position = '';
+                document.body.style.top = '';
+                document.body.style.left = '';
+                document.body.style.right = '';
+                document.body.style.overflow = '';
             }
         };
 
